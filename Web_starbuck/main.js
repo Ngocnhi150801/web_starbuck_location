0dc2ffabe050b8afca6426aecb55868138c78e5f
Mã Hải Nhật <19521953@gm.uit.edu.vn>
2022-12-18 16:16:00 +0700

    gps location

--- Web_starbuck/main.js
+++ Web_starbuck/main.js
@@ -1,6 +1,15 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//           maxZoom: 19,
+//           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//       }).addTo(map);
+
+var lc = L.control
+  .locate({
+    position: "topright",
+    strings: {
+      title: "Show me where I am, yo!"
+    }
+  })
+  .addTo(map);
